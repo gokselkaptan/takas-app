bb85797a300a32dcc2c0716b2e5412d9e5723f51
DeepAgent <dev@takas-a.com>
2026-02-23 23:58:47 +0000

    fix: remove outputFileTracingRoot for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -17,4 +10,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-// Vercel deployment fix - Mon Feb 23 20:51:35 UTC 2026
